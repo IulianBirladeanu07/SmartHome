b882194d03f1afc4018729c1e8ed616d6e9b4cdc
IulianBirladeanu <iulian_birladeanu07@yahoo.com>
2025-12-01 12:09:17 +0200

    more bearable

--- main.cpp
+++ main.cpp
@@ -2,23 +2,34 @@
 #include <algorithm>
 
 #include "DeviceFactory.hpp"
+#include "NestFactory.hpp"
+#include "LIFXFactory.hpp"
+#include "RingFactory.hpp"
 #include "PhilipsFactory.hpp"
+
 #include "HomeController.hpp"
 
 int main() {
     HomeController& home = HomeController::getInstance();
     PhilipsFactory philips;
-    
-    auto light = philips.createDevice(DeviceType::LIGHT);
-    auto thermostat = philips.createDevice(DeviceType::THERMOSTAT);
-    
-    if(light) {
-        home.registerDevice(light);
-    } 
-    else if(thermostat) {
-        home.registerDevice(thermostat);
-    }
-    
+    NestFactory nest;
+    LIFXFactory lifx;
+    RingFactory ring;
+
+    auto registerIfValid = [&home](auto device){
+        if(device) home.registerDevice(device);
+    };
+
+    registerIfValid(philips.createDevice(DeviceType::LIGHT));
+    registerIfValid(philips.createDevice(DeviceType::CAMERA));
+
+    registerIfValid(nest.createDevice(DeviceType::THERMOSTAT));
+    registerIfValid(nest.createDevice(DeviceType::CAMERA));
+
+    registerIfValid(ring.createDevice(DeviceType::DOOR_LOCK));
+    registerIfValid(ring.createDevice(DeviceType::CAMERA));
+
+    registerIfValid(lifx.createDevice(DeviceType::LIGHT));
     home.listAllDevices();
 
     return 0;
